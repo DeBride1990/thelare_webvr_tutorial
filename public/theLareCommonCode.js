73904b1d8951a5b99d82b3732c6a9276b19ca7aa
DeBride1990 <38510018+DeBride1990@users.noreply.github.com>
2018-04-18 17:59:24 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -1,12 +1,10 @@
 var vRViewPlayer = function(elementIdName, options) {
   console.log('Creating VR View Player');
-  return new VRView.Player(elementIdName, options);
-}
-https://pasteboard.co/images/Hhf02Eu.jpg/download',
+  return new VRView.Player(elementIdName, options);}
 is_stereo: false,
 var drawingRoom = vRViewPlayer('#first-vr-example', {
   // image: 'assets/dolphins.jpg',
-  image: '',
+  image: 'https://pasteboard.co/images/Hhf02Eu.jpg/download',
   is_stereo: true,
 });
 
