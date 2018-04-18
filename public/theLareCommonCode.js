c8f92567a3fbbf97258cc23165aa08dc91a54ba0
DeBride1990 <38510018+DeBride1990@users.noreply.github.com>
2018-04-18 17:59:55 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -1,7 +1,7 @@
 var vRViewPlayer = function(elementIdName, options) {
   console.log('Creating VR View Player');
   return new VRView.Player(elementIdName, options);}
-is_stereo: false,
+false,
 var drawingRoom = vRViewPlayer('#first-vr-example', {
   // image: 'assets/dolphins.jpg',
   image: 'https://pasteboard.co/images/Hhf02Eu.jpg/download',
